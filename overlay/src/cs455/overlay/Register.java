81118266e21cb9fb0191427d535eff2efa2310c2
semimono <culleneason@gmail.com>
2015-01-23 09:46:59 -0700

    Merged two versions

--- overlay/src/cs455/overlay/Register.java
+++ overlay/src/cs455/overlay/Register.java
@@ -1,9 +1,38 @@
 package cs455.overlay;
 
+import java.util.List;
+import java.util.Random;
+
 /**
  * Created by Cullen on 1/22/2015.
  */
 public class Register {
+
+	List<NodeInfo> nodes;
+	Random rand;
+
+	public Register() {
+		rand = new Random();
+	}
+
+	public void register(NodeInfo node) {
+		boolean match;
+		do {
+			node.id = rand.nextInt(128);
+			match = false;
+			for(NodeInfo n: nodes) {
+				if (n.id == node.id) {
+					match = true;
+					break;
+				}
+			}
+		} while(match);
+		nodes.add(node);
+	}
+
+	public void unregister(NodeInfo node) {
+		nodes.remove(node);
+	}
 
 	public static void main(String[] args) {
 
