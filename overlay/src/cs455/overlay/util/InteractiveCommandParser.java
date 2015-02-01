a574a29714be3b341e81388ec3c9b44f46cb649b
semimono <culleneason@gmail.com>
2015-02-01 15:35:01 -0700

    Updated command parser

--- overlay/src/cs455/overlay/util/InteractiveCommandParser.java
+++ overlay/src/cs455/overlay/util/InteractiveCommandParser.java
@@ -1,6 +1,6 @@
 package cs455.overlay.util;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Cullen on 1/25/2015.
@@ -8,13 +8,22 @@ import java.util.Scanner;
 public class InteractiveCommandParser {
 
 	String usage;
+	Set<String> commands;
 
 	public InteractiveCommandParser(String usage) {
 		this.usage = usage;
+		commands = new HashSet<String>();
+	}
+
+	public void addCommand(String command) {
+		commands.add(command);
 	}
 
 	public String receiveCommand() {
 		Scanner input = new Scanner(System.in);
-		String command = input.nextLine();
+		String command = input.nextLine().trim();
+		if (commands.contains(command))
+			return command;
+		System.err.println("Unknown command: " +command);
 	}
 }
