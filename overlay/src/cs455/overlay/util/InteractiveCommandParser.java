5035c48cce697db1540c2bc4d622e7d1facb8111
semimono <culleneason@gmail.com>
2015-02-02 22:53:26 -0700

    Finished command parser class

--- overlay/src/cs455/overlay/util/InteractiveCommandParser.java
+++ overlay/src/cs455/overlay/util/InteractiveCommandParser.java
@@ -8,22 +8,44 @@ import java.util.*;
 public class InteractiveCommandParser {
 
 	String usage;
-	Set<String> commands;
+	Map<String, String[]> commands;
 
 	public InteractiveCommandParser(String usage) {
 		this.usage = usage;
-		commands = new HashSet<String>();
+		commands = new HashMap<String, String[]>();
 	}
 
-	public void addCommand(String command) {
-		commands.add(command);
+	public void addCommand(String command, String[] args) {
+		commands.put(command, args);
 	}
 
-	public String receiveCommand() {
+	public String[] receiveCommand() {
 		Scanner input = new Scanner(System.in);
-		String command = input.nextLine().trim();
-		if (commands.contains(command))
+		String[] command = input.nextLine().split("\\s");
+		if (command.length < 1 || command[0].equalsIgnoreCase("help")) {
+			showUsage();
+			return null;
+		}
+		if (commands.containsKey(command[0]) && command.length -1 == commands.get(command[0]).length)
 			return command;
-		System.err.println("Unknown command: " +command);
+
+		System.err.print("Unknown command:");
+		for(String s: command) {
+			System.err.print(" " +s);
+		}
+		System.err.println();
+		return null;
+	}
+
+	public void showUsage() {
+		System.out.println(usage);
+		System.out.println("Available Commands:");
+		for(String com: commands.keySet()) {
+			System.out.print("    " + com);
+			for(String arg: commands.get(com)) {
+				System.out.print(" " +arg);
+			}
+			System.out.println();
+		}
 	}
 }
