7e9f58e0a706ab41f2341e9c53f30511f7adca2c
semimono <culleneason@gmail.com>
2015-04-08 12:17:47 -0600

    Finished

--- gigasort/src/Gigasort.java
+++ gigasort/src/Gigasort.java
@@ -8,49 +8,52 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class Gigasort {
+public class GigaSort {
 
-	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+	public static class SortMapper extends Mapper<Object, Text, Long, Long>{
 
-		private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
 
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-			StringTokenizer itr = new StringTokenizer(value.toString());
-			while (itr.hasMoreTokens()) {
-				word.set(itr.nextToken());
-				context.write(word, one);
+			long num = Long.parseLong(value.toString());
+			context.write(num, num);
+		}
+	}
+
+	public static class SortReducer extends Reducer<Long, Long, Long, Long> {
+
+		private long counter = 0;
+
+		public void reduce(Long key, Iterable<Long> values, Context context) throws IOException, InterruptedException {
+			for (IntWritable val : values) {
+				if (counter %1000 == 0)
+					context.write(key, key);
+				++counter;
 			}
 		}
 	}
 
-	public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
-
-		private IntWritable result = new IntWritable();
-		private long counter = 0;
-
-		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-			int sum = 0;
-			for (IntWritable val : values) {
-				sum += val.get();
-			}
-			result.set(sum);
-			context.write(key, result);
+	public static class SortPartitioner extends Partitioner<Long, Long> {
+		public int getPartition(Long key, Long value, int numReduceTasks) {
+			long partitionSize = Long.MAX_VALUE /numReduceTasks;
+			return (int) (key /partitionSize);
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-		Job job = Job.getInstance(conf, "word count");
-		job.setJarByClass(Gigasort.class);
-		job.setMapperClass(TokenizerMapper.class);
-		job.setCombinerClass(IntSumReducer.class);
-		job.setReducerClass(IntSumReducer.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+		Job job = Job.getInstance(conf, "GigaSort");
+		job.setJarByClass(GigaSort.class);
+		job.setMapperClass(SortMapper.class);
+		job.setReducerClass(SortReducer.class);
+		job.setPartitionerClass(SortPartitioner.class);
+		job.setOutputKeyClass(Long.class);
+		job.setOutputValueClass(Long.class);
+		job.setNumReducerTasks(32);
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
