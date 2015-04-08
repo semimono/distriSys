944f2324fd359933f3cdd1dea67d72f64d1adb69
semimono <culleneason@gmail.com>
2015-04-08 13:33:37 -0600

    Changed to LongWritable

--- gigasort/src/GigaSort.java
+++ gigasort/src/GigaSort.java
@@ -3,7 +3,7 @@ import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -14,22 +14,22 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class GigaSort {
 
-	public static class SortMapper extends Mapper<Object, Text, Long, Long>{
+	public static class SortMapper extends Mapper<Object, Text, LongWritable, LongWritable>{
 
 		private Text word = new Text();
 
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-			long num = Long.parseLong(value.toString());
+			LongWritable num = new LongWritable(Long.parseLong(value.toString()));
 			context.write(num, num);
 		}
 	}
 
-	public static class SortReducer extends Reducer<Long, Long, Long, Long> {
+	public static class SortReducer extends Reducer<LongWritable, LongWritable, LongWritable, LongWritable> {
 
 		private long counter = 0;
 
-		public void reduce(Long key, Iterable<Long> values, Context context) throws IOException, InterruptedException {
-			for (Long val : values) {
+		public void reduce(LongWritable key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException {
+			for (LongWritable val : values) {
 				if (counter %1000 == 0)
 					context.write(key, key);
 				++counter;
@@ -37,10 +37,10 @@ public class GigaSort {
 		}
 	}
 
-	public static class SortPartitioner extends Partitioner<Long, Long> {
-		public int getPartition(Long key, Long value, int numReduceTasks) {
+	public static class SortPartitioner extends Partitioner<LongWritable, LongWritable> {
+		public int getPartition(LongWritable key, LongWritable value, int numReduceTasks) {
 			long partitionSize = Long.MAX_VALUE /numReduceTasks;
-			return (int) (key /partitionSize);
+			return (int) (key.get() /partitionSize);
 		}
 	}
 
@@ -51,8 +51,8 @@ public class GigaSort {
 		job.setMapperClass(SortMapper.class);
 		job.setReducerClass(SortReducer.class);
 		job.setPartitionerClass(SortPartitioner.class);
-		job.setOutputKeyClass(Long.class);
-		job.setOutputValueClass(Long.class);
+		job.setOutputKeyClass(LongWritable.class);
+		job.setOutputValueClass(LongWritable.class);
 		job.setNumReduceTasks(32);
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
